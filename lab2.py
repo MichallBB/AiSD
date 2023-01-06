61feeb98e070af47c0989d9d6dc9df27d9b85678
MichallBB <98035204+MichallBB@users.noreply.github.com>
2023-01-06 01:40:38 +0100

    Add files via upload

--- lab2.py
+++ lab2.py
@@ -1,12 +1,13 @@
 from typing import Any
 
+
 class Node:
     value: any
     next: 'Node'
 
     def __init__(self, value: Any, next=None):
         self.value = value
-        self.next = None
+        self.next = next
 
     def getNext(self):
         return self.next
@@ -23,63 +24,93 @@ class LinkedList:
         self.head = None
         self.tail = None
 
+    def __str__(self):
+        text = ''
+        current = self.head
+        if current != None:
+            text += str(current.value)
+            current = current.next
+            while current.next != None:
+                current = current.next
+                text += " -> " + str(current.value)
+        return text
+
     def push(self, value):
         new_node = Node(value)
+        if self.head == None:
+            self.head = self.tail = Node(value)
         new_node.next = self.head
         self.head = new_node
 
     def append(self, value):
         current = self.head
         if current:
-            while current.getNext() != None:
-                current = current.getNext()
+            while current.next != None:
+                current = current.next
             current.setNext(Node(value))
         else:
             self.head = Node(value)
-        self.tail = current.getNext()
+            current = self.head
+        self.tail = current.next
 
     def node(self, at: int):
         pointer = self.head
         counter = 0
-        while True:
-            if counter == at or pointer.next is None:
-                return pointer
-            counter += 1
+        if at == 0:
+            return pointer
+        while counter < at:
             pointer = pointer.next
+            counter += 1
+        return pointer.next
 
     def insert(self, value: Any, after: Node):
-        pointer = self.head
-        counter = 0
-        while True:
-            if counter == after:
-                node = Node(value)
-                node.next = pointer.next
-                pointer.next = node
-                return None
-            if pointer.next is None:
-                return
-            counter += 1
-            pointer = pointer.next
+        nnode = Node(value)
+        pointer = after
+        if self.head == after:
+            pointer = after.next
+        if pointer.next == None:
+            self.tail = nnode
+        nnode.next = pointer.next
+        pointer.next = nnode
+
     def pop(self):
         pointer = self.head
-        self.head = pointer.next
-        return pointer
+        if self.tail == pointer.next:
+            self.head = None
+            self.tail = None
+        else:
+            self.head = pointer.next
+            return pointer
+
     def remove_last(self):
         pointer = self.head
         last = self.tail
-        while True:
+        if self.tail == pointer.next:
             temp = pointer.next
-            if temp.value == last.value:
-                break
-            pointer = pointer.next
-        pointer.next = None
-        self.tail = pointer
+            self.head = None
+            self.tail = None
+            return temp
+        else:
+            while True:
+                temp = pointer.next
+                if temp.value == last.value:
+                    break
+                pointer = pointer.next
+            temp = pointer.next
+            pointer.next = None
+            self.tail = pointer
+            return temp
+
     def remove(self, after: Node) -> Any:
-        pointer = self.head
-        while True:
-            if pointer.next is after:
-                pointer.next = after.next
-                return None
+        pointer = after.next
+        if pointer.next == None:
+            self.tail = after
+            after.next = None
+            return
+        pointer = pointer.next
+        after.next = pointer
+        return
+
 
     def len(self):
         pointer = self.head
@@ -89,19 +120,6 @@ class LinkedList:
                 return counter
             counter += 1
             pointer = pointer.next
-
-
-lis = LinkedList()
-lis.push(3)
-lis.push(2)
-lis.push(1)
-lis.append(4)
-lis.append(5)
-lis.append(6)
-lis.insert(23,2)
-test = lis.node(0)
-lis.pop()
-lis.remove_last()
-#lis.remove(1)
-#print(test.value)
-print(lis.len())
+    def test(self):
+        pointer = self.head
+        return pointer.next
